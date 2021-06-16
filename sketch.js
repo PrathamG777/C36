de5cf7e86698a523cab643788da87a8a1682c144
PrathamG777 <81462817+PrathamG777@users.noreply.github.com>
2021-06-16 20:21:46 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,22 @@
+var database;
+var canvas, bgImg;
+var gameState = 0;
+var playerCount;
+var form, player, game;
+
+
+function setup(){
+  canvas = createCanvas(400,400);
+  database = firebase.database();
+  game = new Game();
+  game.getState();
+  game.start();
+
+}
+
+function draw(){
+  background("white");
+  
+}
+
+
